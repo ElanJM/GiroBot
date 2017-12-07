9a5c9372543de9bd6d167113c1d0c105edaaf56a
ElanJM <elanjihad2003@gmail.com>
2017-12-07 10:12:53 +0700

    Update test_send_text_message.py

--- tests/api/test_send_text_message.py
+++ tests/api/test_send_text_message.py
@@ -29,7 +29,7 @@ from linebot.models import (
 
 class TestSendTestMessage(unittest.TestCase):
     def setUp(self):
-        self.tested = LineBotApi('channel_secret')
+        self.tested = LineBotApi('bc1445fa31789d24b3cebe96a69b5010')
 
         # test data
         self.text_message = TextSendMessage(text='Hello, world')
